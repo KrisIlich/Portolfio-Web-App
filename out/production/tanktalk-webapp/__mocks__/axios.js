2d212773f79689bfa3e82430ec3a6268ce6e11eb
Kristopher Ilich <kristopherilich@gmail.com>
2025-01-31 15:54:44 -0500

    refactoring

--- out/production/tanktalk-webapp/__mocks__/axios.js
+++
@@ -1,3 +0,0 @@
-export default {
-    post: jest.fn(() => Promise.resolve({ data: { choices: [{ text: "Mocked response" }] } })),
-};
--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,11 @@
 import '../assets/styles/App.css';
-import React, { useEffect, useState} from 'react';
 import { BrowserRouter, Routes, Route, Navigate}  from "react-router-dom";
-import { onAuthStateChanged } from 'firebase/auth';
 import LoginPage from './LoginPage';
 import ChatBot from './ChatBot/ChatBot';
 import HomePage from "./HomePage";
 
 
 function App() {
-
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-
 
   return (
       <BrowserRouter>
